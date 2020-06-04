d8d75229ed13385dce992e1280e26bc9c589f59e
Nick Buker <nickbuker@gmail.com>
2020-06-04 11:46:35 -0700

    add uniqueness type checks and tests

--- tests/test_unique_features.py
+++ tests/test_unique_features.py
@@ -1,5 +1,7 @@
 # third party imports
+import numpy as np
 import pandas as pd
+import pytest
 # local imports
 import whatdis.unique_features as uf
 
@@ -8,11 +10,18 @@ import whatdis.unique_features as uf
 good = pd.DataFrame.from_dict({
     'g1': ('foo', 'bar', 'baz', 'boo'),
     'g2': (0, 1, 2, 4),
-    'g3': ('I', 'like', 'peperoni', 'pizza'),
+    'g3': (np.datetime64('2017-01-01'), np.datetime64('2018-01-01'),
+           np.datetime64('2019-01-01'), np.datetime64('2020-01-01')),
+})
+
+# should fail dtype test
+bad_type = pd.DataFrame.from_dict({
+    'b1': (0.0, 1.0, 2.0, 4.0),
+    'b2': ('foo', 'bar', 'baz', 'boo'),
 })
 
 # should fail duplicates test
-bad = pd.DataFrame.from_dict({
+bad_unique = pd.DataFrame.from_dict({
     'b1': (0, 1, 0, 1),
     'b2': ('foo', 'bar', 'baz', 'boo'),
 })
@@ -21,21 +30,35 @@ bad = pd.DataFrame.from_dict({
 fail_series_bool = pd.Series([True, False], index=['b1', 'b2'])
 fail_series_counts = pd.Series([2, 0], index=['b1', 'b2'])
 
+
+def test_validate_dtype_good():
+    # verifies good data passes the dtype check
+    uf.validate_dtype(good)
+
+
+def test_validate_dtype_bad():
+    # checks that TypeError is raised when df contains float data
+    with pytest.raises(TypeError) as excinfo:
+        uf.validate_dtype(bad_type)
+    # verifies TypeError contains appropriate message
+    assert 'should be of type object, int64, or datetime64' in str(excinfo.value)
+
+
 def test_check_uniqueness_good_bool():
-    # TODO comment
+    # verifies that good data passes uniqueness check (bool)
     assert not uf.check_uniqueness(good, counts=False).any()
 
 
 def test_check_uniqueness_good_counts():
-    # TODO comment
+    # verifies that good data passes uniqueness check (counts)
     assert uf.check_uniqueness(good, counts=True).max() == 0
 
 
 def test_check_uniques_bad_bool():
-    # TODO comment
-    assert uf.check_uniqueness(bad, counts=False).equals(fail_series_bool)
+    # verifies that the uniqueness check find duplicates (bool)
+    assert uf.check_uniqueness(bad_unique, counts=False).equals(fail_series_bool)
 
 
 def test_check_uniques_bad_counts():
-    # TODO comment
-    assert uf.check_uniqueness(bad, counts=True).equals(fail_series_counts)
+    # verifies that the uniqueness check finds duplicates (counts)
+    assert uf.check_uniqueness(bad_unique, counts=True).equals(fail_series_counts)
--- whatdis/unique_features.py
+++ whatdis/unique_features.py
@@ -1,10 +1,20 @@
 # third party imports
+import numpy as np
 import pandas as pd
 
 
-def type_check():
-    # TODO: add type check for bools, floats, datetime, etc.
-    pass
+def validate_dtype(df: pd.DataFrame) -> None:
+    """TODO
+
+    Args:
+        df:
+
+    Returns:
+
+    """
+    if not df.dtypes.isin((np.dtype('O'), np.dtype(int), np.dtype('datetime64[ns]'))).all():
+        raise TypeError('Unique feature columns should be of type object, int64, or datetime64.')
+    return
 
 
 def check_uniqueness(df: pd.DataFrame, counts: bool = False) -> pd.Series:
@@ -17,7 +27,7 @@ def check_uniqueness(df: pd.DataFrame, counts: bool = False) -> pd.Series:
     Returns:
 
     """
-    # TODO add type check and null check
+    validate_dtype(df)
     dupes = df.nunique(axis=0).subtract(df.shape[0]).multiply(-1)
     if not counts:
         dupes = dupes.astype(bool)
