c7a3fb1b4eaa11c4a6d10baf5b46176fb7a86e46
Nick Buker <nickbuker@gmail.com>
2020-06-15 11:48:32 -0700

    add result to df

--- whatdis/utils.py
+++ whatdis/utils.py
@@ -1,13 +1,49 @@
+# TODO docstrings
 # standard library imports
-from typing import Any
+from typing import Any, Union
 # third party imports
 import pandas as pd
 
 
 def check_if_df(data: Any) -> bool:
+    """
+
+    Args:
+        data:
+
+    Returns:
+
+    """
     if isinstance(data, pd.DataFrame):
         return True
     if isinstance(data, pd.Series):
         return False
     else:
         raise TypeError('Input data must be of type pandas DataFrame or pandas Series.')
+
+
+def result_to_df(
+        data: Union[pd.Series, bool, float, int],
+        title: str,
+        **kwargs,
+) -> pd.DataFrame:
+    """
+
+    Args:
+        data:
+        title:
+        **kwargs:
+
+    Returns:
+
+    """
+    if isinstance(data, pd.Series):
+        d = {'column': data.index, title: data.values}
+        if kwargs:
+            kwargs = {k: (v,) * data.shape[0] for k, v in kwargs.items()}
+    else:
+        d = {'check': (title,), 'result': (data,)}
+        if kwargs:
+            kwargs = {k: (v,) for k, v in kwargs.items()}
+    d.update(kwargs)
+    return pd.DataFrame.from_dict(d)
